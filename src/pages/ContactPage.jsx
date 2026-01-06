f89d7c4b40627ddaca2f0f4a45eff4358997344b
JAM3S11 <jdndirangu2020@gmail.com>
2026-01-07 00:10:09 +0300

    Update commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,4 @@
+import { Mail, Phone } from 'lucide-react'
 import React from 'react'
 
 const ContactPage = () => {
@@ -22,8 +23,44 @@ const ContactPage = () => {
                         Let's Connect
                     </p>
                     <p className='text-gray-700 dark:text-gray-400 text-lg leading-relaxed mb-8 max-w-md'>
-                    I would love to hear from you and how I could help. Please fill in the form and I'll get back to you as soon as possible.
+                    I would love to hear from you and how I could help. Please fill in the form and I'll get back to you as soon as possible. As well as feel free to reach.
                     </p>
+
+                    <div className='space-y-4'>
+                        <div className='flex items-center gap-4 bg-slate-300 dark:bg-[#111827]/50 border border-gray-200 dark:border-gray-800 p-5 rounded-2xl hover:border-blue-500/50 transition-all duration-300 group'>
+                            <div className='w-10 h-10 rounded-lg text-gray-700 dark:text-gray-400 bg-[#c0bed8] dark:bg-[#19183B]/80 flex items-center justify-center'>
+                                <Mail size={25} />
+                            </div>
+                            <div>
+                                <p className='text-[15px] uppercase tracking-widest font-bold text-gray-600 dark:text-gray-300'>
+                                    Mail
+                                </p>
+                                <p className='font-semibold text-gray-600 dark:text-gray-400'>
+                                    jdndirangu2020@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                        <div className='flex items-center gap-4 bg-slate-300 dark:bg-[#111827]/50 border border-gray-200 dark:border-gray-800 p-5 rounded-2xl hover:border-blue-500/50 transition-all duration-300 group'>
+                            <div className='w-10 h-10 flex items-center justify-center text-gray-700 dark:text-gray-400 bg-[#c0bed8] dark:bg-[#19183B]/80 rounded-lg'>
+                                <Phone size={25} />
+                            </div>
+                            <div>
+                                <p className='text-[15px] uppercase tracking-widest font-bold text-gray-600 dark:text-gray-300'>
+                                    Phone
+                                </p>
+                                <p className='font-semibold text-gray-600 dark:text-gray-400'>
+                                    +254 716 041419
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='space-y-3'>
+                        <p className='font-bold text-base text-gray-700 dark:text-gray-400 mb-2'>
+                            Connect on social
+                        </p>
+                        <div className='grid grid-cols-3 gap-4'></div>
+                    </div>
                 </div>
             </div>
         </div>
